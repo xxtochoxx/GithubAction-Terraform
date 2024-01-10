453ebd847de30b193362e36049791a6487f9f175
Humberto Melendez <humberto.melendez@globant.com>
2024-01-10 10:39:42 -0500

    python arbol

---
+++ arbol.py
@@ -0,0 +1,9 @@
+#Primer bucle, range(start,stop,step)
+for i in range(1,30,2):
+print(('^'*i).center(30))
+ 
+for leg in range(3):
+print(('|||').center(30))
+print(('\_____/').center(30))
+print(' '+30*'-')
+print('(<em>¨</em>¨<em>¨</em>)¡Feliz Navidad! (<em>¨</em>¨<em>¨</em>)')
